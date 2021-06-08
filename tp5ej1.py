f37d985f1fe0eb83fb493bfec54c503275b07bf1
Adrian Evaraldo <adrievar@gmail.com>
2021-06-08 20:24:22 -0300

    Ejercicios 1 y 2 a medias
    Se subió la función de prueba del ejercicio 1 y el esqueleto del segundo ejercicio

--- tp5ej1.py
+++ tp5ej1.py
@@ -4,13 +4,29 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
+import tp4ej1.py as inp
 
-# Reemplazar por las funciones del ejercicio
-
+def es_par(numero):
+    """
+    Función que retorna True si el numero ingresado es par y False en caso contrario
+    """
+    pass
 
 def prueba():
-    """Toda la interacción con el usuario va acá"""
-    pass
+    while True:
+        print('\033[2J')
+        print("""
+En este ejercicio, se le tiene que ingresar un numero
+y la funcion retorna True si el numero es par y 
+False si el numero es impar
+    Ingrese 1 para ingresar número
+    Ingrese 2 para terminar la prueba""")
+
+        test = inp.ingreso_entero_restringido("ingrese opción", 1, 2)
+        if test == 1:
+            sleep(10)
+        elif test == 2:
+            break
 
 if __name__ == "__main__":
     prueba()
