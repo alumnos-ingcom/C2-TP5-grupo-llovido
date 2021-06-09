88089ae03db97e41af115eb0eec08567e9962c02
Adrian Evaraldo <adrievar@gmail.com>
2021-06-09 00:05:00 -0300

    ejercicio 2
    Hecha la función base de la sucesión de fibonacci

--- tp5ej2.py
+++ tp5ej2.py
@@ -4,9 +4,18 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
+class IteracionDebajoRango(Exception):
+    """
+    Excepcion en el caso de que el numero ingresado sea menor o igual a 2
+    """
+    pass
 
-# Reemplazar por las funciones del ejercicio
-
+def fibonacci(iteracion): 
+    if iteracion == 0 or iteracion == 1:
+        ret = 1
+    else:
+        ret = fibonacci(n - 1) + fibonacci(n -2)
+    return ret
 
 def prueba():
     """Toda la interacción con el usuario va acá"""
