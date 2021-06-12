0dff90386d63cbd23deea00629895191d9a0da4c
AgostinaB <biagini.ag@gmail.com>
2021-06-12 17:02:05 -0300

    Ej 4 finalizado (para revisar)
    Ej 4 funciona, está para control final

--- tp5ej4.py
+++ tp5ej4.py
@@ -4,7 +4,7 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
-import tp4ej1.py as inp
+import tp4ej1 as inp
 from time import sleep
 
 
@@ -20,11 +20,40 @@ def numeros_perfectos(numero):
         ret(bool):      Retorna True si el numero es perfecto
                         y False en caso contrario
     '''
-
+    suma = 0
+    for i in range(1, numero):
+        if numero % i == 0:
+            suma = suma + i
+        print(i , numero, suma)
+    if numero == suma:
+        return True
+    else:
+        return False
+        
+        
+        
 def prueba():
     """Toda la interacción con el usuario va acá"""
-    pass
+    while True:
+        print('\033[2J')
+        print("""
+En este ejercicio, se le tiene que ingresar un numero
+y la funcion retorna True si el numero es perfecto y 
+False si el numero no lo es.
+    Ingrese 1 para ingresar número
+    Ingrese 2 para terminar la prueba""")
 
+        test = inp.ingreso_entero_restringido("ingrese opción", 1, 2)
+        if test == 1:
+            n = inp.ingreso_entero("ingrese numero")
+            resu = numeros_perfectos(n)
+            print(f'La evaluación de {n} como perfecto resultó: {resu}')
+            sleep(10)
+        elif test == 2:
+            break
+        
+        
+        
 if __name__ == "__main__":
     prueba()
 
