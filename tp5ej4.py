f290ec7e35ccc71b2908e23fcb3e28c690802212
Adrian Evaraldo <adrievar@gmail.com>
2021-06-12 16:38:58 -0300

    agregado ejercicio 4

--- tp5ej4.py
+++ tp5ej4.py
@@ -6,8 +6,20 @@
 
 import tp4ej1.py as inp
 from time import sleep
-# Reemplazar por las funciones del ejercicio
 
+
+
+def numeros_perfectos(numero):
+    '''
+    Funcion que determina si un numero es o no perfecto.
+
+    argumentos:
+        numero(int):    numero a ser probado
+
+    retorna:
+        ret(bool):      Retorna True si el numero es perfecto
+                        y False en caso contrario
+    '''
 
 def prueba():
     """Toda la interacción con el usuario va acá"""
